87364c4154efebde1c2f8d45c877c00eb7062075
fdavison1 <fred.noreply@gmail.com>
2020-11-07 20:23:25 -0600

    stub

---
+++ src/headers/header-large.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const HeaderLarge = () => {
+    return (
+        <header className='header-large'>
+            <div className='inner'>
+                <h1>doily dev</h1>
+                <div className='subheadings'>
+                    <h2>PROJECTS</h2>
+                    {/* <h2>PROJECTS</h2>
+                    <h2>PROJECTS</h2> */}
+                    <div className='bar'></div>
+                </div>
+            </div>
+        </header>
+    )
+}
