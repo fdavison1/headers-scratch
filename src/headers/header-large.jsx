90dfd56651e6bf11a5ed0c7363f024cd2af48b48
fdavison1 <fred.noreply@gmail.com>
2020-11-10 14:15:51 -0600

    update title

--- src/headers/header-large.jsx
+++ src/headers/header-large.jsx
@@ -7,6 +7,9 @@ export const HeaderLarge = () => {
                 <h1>doily dev</h1>
                 <div className='subheadings'>
                     <h2>PROJECTS</h2>
+                    {/* <h2>GITHUB</h2> */}
+                    {/* <h2>LINKEDIN</h2> */}
+                    {/* <h2>ABOUT</h2> */}
                     {/* <h2>PROJECTS</h2>
                     <h2>PROJECTS</h2> */}
                     <div className='bar'></div>
