5ef3ffcd2f8d02ce8c4962c1d42b8c9dd9e95579
fdavison1 <fred.noreply@gmail.com>
2020-11-11 21:40:45 -0600

    mockup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './sass/App.scss';
 import {HeaderLarge} from './headers/header-large';
+import {SpinningDoily} from './components/SpinningDoily';
+import {Main} from './Main';
 
 function App() {
   return (
     <div className="App">
         <HeaderLarge/>
+        <Main/>
     </div>
   );
 }
